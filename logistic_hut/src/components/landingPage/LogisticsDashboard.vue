807edd3ad451164cbedf0ac085fb94fbe35df311
nwobodokenneth <k.nwobodo@semicolon.africa>
2023-03-11 21:21:12 +0100

    first changes

---
+++ logistic_hut/src/components/landingPage/LogisticsDashboard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Welcome to the dashboard
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LogisticsDashboard"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ logistic_hut/src/views/LandingPageViews/DashboardViews.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <LogisticsDashboard/>
+  </div>
+</template>
+
+<script>
+import LogisticsDashboard from "@/components/landingPage/LogisticsDashboard";
+export default {
+  name: "DashboardViews",
+  components: {LogisticsDashboard}
+}
+</script>
+
+<style scoped>
+
+</style>
